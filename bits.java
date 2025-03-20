55987861bf41f5ef8c0f5c0ecd0de7faf0a46bf5
NaveenSuryavanshi <suryavanshinaveen59@gmail.com>
2025-03-20 16:21:15 +0530

    sorting

--- bits.java
+++ bits.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
 // public class bits{
 //     public static void main(String[] args){
@@ -29,14 +29,14 @@ import java.util.Scanner;
 
 // clear bit
 
-public class bits{
-    public static void main(String[] args){
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        int pos = s.nextInt();
-        int bitmask = 1 << pos;
-        int notbitmask = ~(bitmask);
-        notbitmask = n & notbitmask;
-        System.out.println(notbitmask);
-    }
-}
+// public class bits{
+//     public static void main(String[] args){
+//         Scanner s = new Scanner(System.in);
+//         int n = s.nextInt();
+//         int pos = s.nextInt();
+//         int bitmask = 1 << pos;
+//         int notbitmask = ~(bitmask);
+//         notbitmask = n & notbitmask;
+//         System.out.println(notbitmask);
+//     }
+// }
---
+++ sorting.java
@@ -0,0 +1,23 @@
+// public class Sorting{
+//     public static void prinitAray(int[] arr){
+//         for( int i = 9 ; i<arr.length ; i++){
+//             System.out.println();
+//         }
+//     }
+// }
+// public static void main(String[] args){
+//     int[] arr = {10,12,14,56,19};
+//     for(int i = 0 ; i<arr.length ; i++){
+//     for(int j = 0 ; j<arr.length-i-1 ; j++){
+//         if(arr[j]>arr[j+1]){
+//             int temp = arr[j];
+//             arr[j] = arr[j+1];
+//             arr[j+1] = temp;
+//         }
+//     }
+// }
+// prinitAray(arr);
+// }
+
+
+
