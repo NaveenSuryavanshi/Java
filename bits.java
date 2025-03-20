85831efe63561712b1263a72eeacf3ce1854db00
NaveenSuryavanshi <suryavanshinaveen59@gmail.com>
2025-03-20 15:12:10 +0530

    20mar bit comp

--- bits.java
+++ bits.java
@@ -1,16 +1,42 @@
 import java.util.Scanner;
 
+// public class bits{
+//     public static void main(String[] args){
+//         Scanner s = new Scanner(System.in);
+//         int n = s.nextInt();
+//         int pos = s.nextInt();
+//         int bitmask = 1 << pos;
+//         int bit = n & bitmask;
+//         if(bit != 0){
+//             System.out.println("1");
+//         }else{
+//             System.out.println("0");
+//         }
+
+
+// set bit
+// public class bits{
+//     public static void main(String[] args){
+//         Scanner s = new Scanner(System.in);
+//         int n = s.nextInt();
+//         int pos = s.nextInt();
+//         int bitmask = 1 << pos;
+//         int bit = n | bitmask;
+//         System.out.println(bit);
+//     }
+// }
+
+
+// clear bit
+
 public class bits{
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int pos = s.nextInt();
         int bitmask = 1 << pos;
-        int bit = n & bitmask;
-        if(bit != 0){
-            System.out.println("1");
-        }else{
-            System.out.println("0");
-        }   
+        int notbitmask = ~(bitmask);
+        notbitmask = n & notbitmask;
+        System.out.println(notbitmask);
     }
 }
