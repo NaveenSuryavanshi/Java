e246f98399d3f81e2499a2662edbd8ea1d0b9710
NaveenSuryavanshi <suryavanshinaveen59@gmail.com>
2025-03-21 15:14:52 +0530

    21march25

--- bits.java
+++ bits.java
@@ -1,21 +1,25 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class bits{
-//     public static void main(String[] args){
-//         Scanner s = new Scanner(System.in);
-//         int n = s.nextInt();
-//         int pos = s.nextInt();
-//         int bitmask = 1 << pos;
-//         int bit = n & bitmask;
-//         if(bit != 0){
-//             System.out.println("1");
-//         }else{
-//             System.out.println("0");
-//         }
+public class bits{
+    public static void main(String[] args) {
+    update(4, 2, 0);
+    }
+    // public static void main(String[] args){
+    //     Scanner s = new Scanner(System.in);
+    //     int n = s.nextInt();
+    //     int pos = s.nextInt();
+    //     int bitmask = 1 << pos;
+    //     int bit = n & bitmask;
+    //     if(bit != 0){
+    //         System.out.println("1");
+    //     }else{
+    //         System.out.println("0");
+    //     }
+    // }
 
 
 // set bit
-// public class bits{
+
 //     public static void main(String[] args){
 //         Scanner s = new Scanner(System.in);
 //         int n = s.nextInt();
@@ -24,19 +28,36 @@
 //         int bit = n | bitmask;
 //         System.out.println(bit);
 //     }
-// }
+// // }
 
 
 // clear bit
+    // public static void main(String[] args){
+    //     Scanner s = new Scanner(System.in);
+    //     int n = s.nextInt();
+    //     int pos = s.nextInt();
+    //     int bitmask = 1 << pos;
+    //     int notbitmask = ~(bitmask);
+    //     notbitmask = n & notbitmask;
+    //     System.out.println(notbitmask);
+    // }
 
-// public class bits{
-//     public static void main(String[] args){
-//         Scanner s = new Scanner(System.in);
-//         int n = s.nextInt();
-//         int pos = s.nextInt();
-//         int bitmask = 1 << pos;
-//         int notbitmask = ~(bitmask);
-//         notbitmask = n & notbitmask;
-//         System.out.println(notbitmask);
-//     }
-// }
+
+    public static void update(int n, int p, int op){
+        Scanner s = new Scanner(System.in);
+        int mask;
+        if(op==0){
+            mask = 1 <<p;
+            n = n | mask;
+        }
+        else{
+            mask = ~( 1 <<p);
+            n = n & mask;
+        }
+
+        System.out.println(n);
+
+    }
+}
+
+
--- sorting.java
+++ sorting.java
@@ -1,6 +1,6 @@
 // public class Sorting{
 //     public static void prinitAray(int[] arr){
-//         for( int i = 9 ; i<arr.length ; i++){
+//         for( int i = 0 ; i<arr.length ; i++){
 //             System.out.println();
 //         }
 //     }
