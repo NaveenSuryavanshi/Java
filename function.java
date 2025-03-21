40dafad18cad3114cc2a38423cc266803b1a7eb9
NaveenSuryavanshi <suryavanshinaveen59@gmail.com>
2025-03-21 22:58:22 +0530

    function

---
+++ function.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class function {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter first numbers: ");
+        // int num1 = sc.nextInt();
+        // System.out.println("Enter second numbers: ");
+        // int num2 = sc.nextInt();
+        // int sum = add(num1, num2);
+        // System.out.println("The sum of the two numbers is: " + sum);
+
+        // }
+        // public static int add(int a, int b)
+        // {
+        // return a + b;
+        // }
+
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.println("Enter the numbers: ");
+    //     int num = sc.nextInt();
+    //     checkEvenOdd(num);
+    // }
+
+    // public static void checkEvenOdd(int num) {
+    //     if (num % 2 == 0) {
+    //         System.out.println("The number is even");
+    //     } else {
+    //         System.out.println("The number is odd");
+    //     }
+    // }
+
+    // Scanner sc = new Scanner(System.in);
+    // System.out.println("Enter first numbers: ");
+    // int num1 = sc.nextInt();
+    // System.out.println("Enter second numbers: ");
+    // int num2 = sc.nextInt();
+    // System.out.println("Enter third number: ");
+    // int num3 = sc.nextInt();
+    // int findMax = max(num1, num2, num3);
+    // System.out.println("The maximum number is: " + findMax);
+    // }
+    // public static int max(int a, int b, int c){
+    //     return Math.max(a, Math.max(b, c));
+    // }
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number: ");
+    int num = sc.nextInt();
+    if(prime(num)){
+        System.out.println("The number is prime");
+    } else {
+        System.out.println("The number is not prime");
+        public static 
+
+}
+}
